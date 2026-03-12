094a69112cabeb05bad0a3503a78e396c170bf3b
analopez03 <analopezdeahumada97@gmail.com>
2026-03-12 17:43:13 +0100

    docs: añadido

--- portfolio/app/pages/index.vue
+++ portfolio/app/pages/index.vue
@@ -1,8 +1,13 @@
 <template>
-  <div class="container mx-auto px-4 py-8">
-    <h1 class="text-4xl font-bold mb-4">Bienvenido a mi Portafolio</h1>
-    <p class="text-lg mb-6">
-      Explora mis proyectos y habilidades en desarrollo web.
-    </p>
-  </div>
+  <section>
+    <div>
+      <h1>Graphic Designer and Web Developer</h1>
+      <p>
+        Soy un diseñador gráfico y desarrollador web apasionado por crear experiencias digitales únicas y funcionales.
+      </p>
+      <div>
+        <
+      </div>
+    </div>
+  </section>
 </template>
