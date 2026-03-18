64f5ed429c83ea53137dcaf329217d8c5a4fc15f
analopez03 <analopezdeahumada97@gmail.com>
2026-03-18 16:59:28 +0100

    fix: solved bug

---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  modules: ["@nuxt/a11y", "@nuxtjs/tailwindcss", "@pinia/nuxt", "@nuxt/image"],
+
+  css: ["~/assets/css/reset.css"],
+});
