621a66a819c225c6baa6e91138a7e6e924a64f11
analopez03 <analopezdeahumada97@gmail.com>
2026-03-18 16:37:47 +0100

    fix: remove card change

---
+++ portfolio/app/components/SkillsSection.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+import TheTitle from "./TheTitle.vue";
+
+const skills = [
+  {
+    title: "Diseño",
+    items: [
+      { name: "Figma", level: 95 },
+      { name: "Adobe XD", level: 95 },
+      { name: "Illustrator", level: 95 },
+      { name: "Photoshop", level: 90 },
+    ],
+  },
+  {
+    title: "Desarrollo",
+    items: [
+      { name: "React", level: 65 },
+      { name: "TypeScript", level: 70 },
+      { name: "JavaScript", level: 78 },
+      { name: "HTML", level: 90 },
+    ],
+  },
+  {
+    title: "Herramientas",
+    items: [
+      { name: "Git", level: 90 },
+      { name: "Wordpress", level: 90 },
+      { name: "Tailwind CSS", level: 60 },
+      { name: "Bootstrap", level: 90 },
+    ],
+  },
+];
+</script>
+
+<template>
+  <section class="py-20 px-6 max-w-7xl mx-auto">
+    <TheTitle>
+      Mis
+      <template #pinkTitle>Habilidades</template>
+    </TheTitle>
+
+    <div class="grid md:grid-cols-3 gap-8">
+      <div
+        v-for="category in skills"
+        :key="category.title"
+        class="bg-gray-100 p-8 rounded-2xl shadow-sm"
+      >
+        <h3 class="text-xl font-semibold mb-6">
+          {{ category.title }}
+        </h3>
+
+        <div v-for="skill in category.items" :key="skill.name" class="mb-6">
+          <div class="flex justify-between text-sm mb-2">
+            <span class="text-gray-600">{{ skill.name }}</span>
+            <span class="text-gray-950 font-semibold">{{ skill.level }}%</span>
+          </div>
+
+          <div class="w-full h-2 bg-gray-300 rounded-full">
+            <div
+              class="h-2 rounded-full bg-gradient-to-r from-purple-500 to-pink-500"
+              :style="{ width: skill.level + '%' }"
+            ></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
---
+++ portfolio/app/layouts/header-footer.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="min-h-screen flex flex-col">
+    <header class="bg-pink-50 px-5 grid grid-cols-9 items-stretch h-20">
+      <div class="col-span-4 flex justify-center items-center">
+        <nuxt-link to="/">
+          <NuxtImg src="/img/postgre.svg" alt="Logo" class="h-12 w-auto" />
+        </nuxt-link>
+        <h1 class="ml-2">Ana López De Ahumada</h1>
+      </div>
+
+      <nav class="col-span-5 flex justify-center items-stretch mr-8">
+        <NuxtLink
+          to="/"
+          class="flex items-center px-6 hover:bg-pink-100 transition-colors"
+        >
+          Home
+        </NuxtLink>
+
+        <nuxt-link
+          to="#contact"
+          class="flex items-center px-6 hover:bg-pink-100 transition-colors"
+        >
+          Contacto
+        </nuxt-link>
+      </nav>
+    </header>
+
+    <main class="flex-grow">
+      <slot />
+    </main>
+
+    <footer class="bg-pink-50 text-center py-4" id="contact" role="contentinfo">
+      <p>&copy; 2026 Portafolio. Todos los derechos reservados.</p>
+      <SocialNetwork />
+    </footer>
+  </div>
+</template>
